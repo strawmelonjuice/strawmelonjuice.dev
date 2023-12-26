08f64779aafd7dbc623a1db56e9927761ff3fc8d
Mar <mar@strawmelonjuice.com>
2023-12-26 02:02:10 +0100

    Recreated the imgmoji

--- cynthiaFiles/assets/scripts/client.js
+++ cynthiaFiles/assets/scripts/client.js
@@ -142,3 +142,241 @@ switch (pagemetainfo.mode) {
         }
         break;
 }
+{
+    function mediamobilescreen() {
+        return ((window.matchMedia("(orientation: portrait),(max-width: 800px)")).matches);
+    };
+
+    function colorSchemeChange(x) {
+        if (document.body.classList.contains("colorSchemeOverridden")) { return }
+        if (x.matches) {
+            document.body.classList.add("dark-theme");
+            document.body.classList.remove("light-theme");
+            document.body.dataset.colorscheme = 0;
+        } else {
+            document.body.classList.add("light-theme");
+            document.body.classList.remove("dark-theme");
+            document.body.dataset.colorscheme = 0;
+        }
+    }
+
+
+    var x = window.matchMedia("(prefers-color-scheme: dark)")
+    document.body.classList.add("dark-theme");
+    document.body.classList.add("light-theme");
+    colorSchemeChange(x);
+
+    x.addListener(colorSchemeChange);
+
+    function colorSchemeOverride(to = 1 | 2 | 0) {
+        switch (to) {
+            case 1:
+                document.body.classList.add("dark-theme");
+                document.body.classList.add("colorSchemeOverridden");
+                document.body.classList.remove("light-theme");
+                document.body.dataset.colorscheme = 1;
+                localStorage.setItem('ForceColorScheme', 1);
+                break;
+            case 2:
+                document.body.classList.add("light-theme");
+                document.body.classList.add("colorSchemeOverridden");
+                document.body.classList.remove("dark-theme");
+                document.body.dataset.colorscheme = 2;
+                localStorage.setItem('ForceColorScheme', 2);
+                break;
+            case 0:
+                document.body.classList.remove("colorSchemeOverridden");
+                colorSchemeChange(x)
+                document.body.dataset.colorscheme = 0;
+                localStorage.setItem('ForceColorScheme', 0);
+                break;
+        }
+        UpdateColorSchemeToggle()
+    }
+    function UpdateColorSchemeToggle() {
+        if (document.body.classList.contains("colorSchemeOverridden")) {
+            document.getElementById("daylighttoggleimg").src = "/assets/img/png/strawberry-sun-moon.png";
+            document.getElementById("daylighttoggleimg").alt = "Auto light/dark";
+            document.getElementById("daylighttoggleimg").setAttribute("onclick", "colorSchemeOverride(0)");
+        } else {
+            if (x.matches) {
+                document.getElementById("daylighttoggleimg").src = "/assets/img/png/strawberry-sun.png";
+                document.getElementById("daylighttoggleimg").alt = "Light mode";
+                document.getElementById("daylighttoggleimg").setAttribute("onclick", "colorSchemeOverride(2)");
+            } else {
+                document.getElementById("daylighttoggleimg").src = "/assets/img/png/strawberry-moon.png";
+                document.getElementById("daylighttoggleimg").alt = "Dark mode";
+                document.getElementById("daylighttoggleimg").setAttribute("onclick", "colorSchemeOverride(1)");
+            }
+        }
+    }
+    setInterval(function () { UpdateColorSchemeToggle() }, 1000);
+
+    function unrollbottombar() {
+        var x = document.getElementById("mybottombar");
+        if (x.className === "bottombar") {
+            x.className += " responsive";
+        } else {
+            x.className = "bottombar";
+        }
+        console.log("fn unrollbottombar() triggered")
+    }
+    function permabigbadgies() {
+        el = (document.getElementsByClassName('badgearea')[0])
+        if (typeof (el) != 'undefined' && (el) != null) {
+            el.style.minHeight = "fit-content";
+            bigbadgies();
+            return 1;
+        } else return 0;
+    }
+    function openNav() {
+        document.getElementById("mySidebar").style.width = "70vw";
+        setTimeout(permabigbadgies, 2500);
+        elem = document.getElementsByClassName("search-button")[0];
+        if (typeof (elem) != 'undefined' && (elem) != null) {
+            elem.classList.add("not-on-mobile");
+            return 1;
+        }
+        // (document.getElementsByClassName("content")[0]).style.marginLeft = "250px";
+    }
+
+    function closeNav() {
+        document.getElementById("mySidebar").style.width = "";
+        elem = document.getElementsByClassName("search-button")[0];
+        if (typeof (elem) != 'undefined' && (elem) != null) {
+            elem.classList.remove("not-on-mobile");
+        }
+        // (document.getElementsByClassName("content")[0]).style.marginLeft = "0";
+    }
+    el = (document.getElementsByClassName('badgearea')[0])
+    if (window.innerHeight < 350) {
+        if (typeof (el) != 'undefined' && (el) != null) {
+            el.style.display = "none";
+        }
+
+    }
+    function TouchContentUnfocusfromMenu() {
+        closeNav()
+        document.getElementById("mybottombar").className = "bottombar";
+    }
+
+    var HttpClient = function () {
+        this.get = function (aUrl, aCallback) {
+            var anHttpRequest = new XMLHttpRequest();
+            anHttpRequest.onreadystatechange = function () {
+                if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
+                    aCallback(anHttpRequest.responseText);
+            }
+
+            anHttpRequest.open("GET", aUrl, true);
+            anHttpRequest.send(null);
+        }
+    }
+
+    var client = new HttpClient();
+    client.get('/assets/html/sociallinks.html', function (response) {
+        sociallinkelements = document.getElementsByClassName("sociallinks");
+        for (var i = sociallinkelements.length - 1; i >= 0; i--) {
+            sociallinkelementnew = document.createElement("div");
+            sociallinkelementnew.setAttribute("style", "text-align: center; align-content: center; margin-left: auto; margin-right: auto;");
+            sociallinkelementnew.innerHTML = response;
+            sociallinkelements.item(i).parentNode.replaceChild(sociallinkelementnew, sociallinkelements.item(i));
+        }
+    })
+
+    document.body.style.filter = "none";
+    document.getElementById("filtertoggle").innerHTML = "Contrast/Greyscale";
+    document.getElementById("filtertoggle").style.padding = "10px";
+
+    console.log(accessibilityfilter);
+    switch (accessibilityfilter) {
+        case 'contrast':
+            document.body.style.filter = "contrast(1.6)";
+            document.body.style.backgroundColor = (getComputedStyle(document.body).getPropertyValue('--filter-contrast-backgroundcolor'));
+            document.body.style.backgroundImage = "none";
+            document.getElementById("filtertoggle").innerText = "Greyscale";
+            break;
+        case 'grayscale':
+            document.body.style.filter = "grayscale(1)";
+            document.body.style.backgroundColor = (getComputedStyle(document.body).getPropertyValue('--filter-grayscale-backgroundcolor'));
+            document.body.style.backgroundImage = "none";
+            document.getElementById("filtertoggle").innerText = "Colorful";
+            break;
+        case "none":
+        default:
+            document.body.style.filter = "none";
+            document.body.style.backgroundImage = "";
+            document.body.style.backgroundColor = "";
+            document.getElementById("filtertoggle").innerText = "Contrast";
+            break;
+    }
+
+
+
+    function ToggleFilters() {
+        console.log(document.body.style.filter)
+        switch (document.body.style.filter) {
+            default:
+                document.body.style.filter = "contrast(1.6)";
+                document.body.style.backgroundColor = (getComputedStyle(document.body).getPropertyValue('--filter-contrast-backgroundcolor'));
+                document.body.style.backgroundImage = "none";
+                document.getElementById("filtertoggle").innerHTML = "Greyscale";
+                break;
+            case ("contrast(1.6)"):
+                document.body.style.filter = "grayscale(1)";
+                document.body.style.backgroundColor = (getComputedStyle(document.body).getPropertyValue('--filter-grayscale-backgroundcolor'));
+                document.body.style.backgroundImage = "none";
+                document.getElementById("filtertoggle").innerHTML = "Colorful";
+                break;
+            case ("grayscale(1)"):
+                document.body.style.filter = "none";
+                document.body.style.backgroundColor = "";
+                document.body.style.backgroundImage = "";
+                document.getElementById("filtertoggle").innerHTML = "Contrast";
+                break;
+        }
+    }
+
+    setInterval(function () {
+        if ((typeof (document.getElementById("bmc-wbtn")) !== 'undefined') && ((document.getElementById("bmc-wbtn")) !== null)) {
+            (document.getElementById("bmc-wbtn")).removeAttribute("style");
+            // (document.getElementById("bmc-wbtn")).style.display = "none";
+        } else {
+            console.log("could not find bmc");
+        }
+    }, 3000);
+    setTimeout(function () {
+        if ((typeof (document.getElementById("oneko")) == 'undefined') || ((document.getElementById("oneko")) == null)) {
+            document.getElementById("kittontoggle").remove();
+            console.log("oneko is not here.")
+        } else {
+            if (wantkitton == false) {
+                (document.getElementById("oneko")).style.display = "none";
+                (document.getElementById("kittontoggletext")).innerText = "show!";
+            } else {
+                (document.getElementById("kittontoggletext")).innerText = "hide..";
+            };
+        }
+    }, 1500);
+    function ToggleKitton() {
+        var kitton = (document.getElementById("oneko"));
+        switch (kitton.style.display) {
+            case 'none':
+                kitton.style.display = "";
+                (document.getElementById("kittontoggletext")).innerText = "hide..";
+                break;
+
+            default:
+                kitton.style.display = "none";
+                (document.getElementById("kittontoggletext")).innerText = "show!";
+                break;
+        }
+    }
+    taggos = (document.getElementsByClassName('taggo'))
+    for (var i = taggos.length - 1; i >= 0; i--) {
+        var newtaggo = document.createElement("a");
+        newtaggo.href = "/search?s=%2C+" + taggos.item(i).innerText;
+        newtaggo.innerHTML = "<code class='taggo'>" + taggos.item(i).innerText + "</code>";
+        taggos.item(i).parentNode.replaceChild(newtaggo, taggos.item(i));
+    }
+}
--- cynthiaFiles/pages/index.md
+++ cynthiaFiles/pages/index.md
@@ -1,4 +1,15 @@
-# Hello!
-This is an example page from CynthiaCMS.
+# Good Day!
 
-If you are here because you just set up a new Cynthia instance, we have a page [to get you started](/p/getting/started/)!
+<span class="sociallinks">no scriptos?</span>
+
+Welcome to my cosy little strawberbrain [site](/p/abt-site) <imgmote>:3</imgmote>
+
+
+
+I'm Mar, <hl-img src="https://avatars.githubusercontent.com/u/101558380?s=400&u=aa8f776b3e11f02130575d1b46851cca05a0c981&v=4" style="height: 18px" alt="small Mar self-portrait"><img img src="https://avatars.githubusercontent.com/u/101558380?s=400&u=aa8f776b3e11f02130575d1b46851cca05a0c981&v=4" height="18px" alt="small Mar self-portrait"></hl-img>strawmelonjuice or however you may know me.
+- 📫 How to reach me: `strawmelonjuice` on Discord or [mar@strawmelonjuice.com](mailto:mar@strawmelonjuice.com)
+- 😄 Pronouns: `She/her`
+
+
+
+<a href="/p/links"><button>My important links<imgmote>blurpleheart</imgmote></button></a>
